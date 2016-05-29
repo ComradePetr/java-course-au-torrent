3585785c76c95ae9a8648c8960beae3483de8271
ComradePetr <Zenbook>
2016-05-29 08:56:42 +0300

    Modify test.sh

--- test.sh
+++ test.sh
@@ -22,9 +22,15 @@ JAR=$PROJ/target/torrent-1.0-SNAPSHOT-jar-with-dependencies.jar
 SERVER="$JAVA -cp $JAR ru.spbau.mit.TorrentTrackerMain"
 CLIENT="$JAVA -cp $JAR ru.spbau.mit.TorrentClientMain"
 
+echo SERVER
+
 cd $TMP/server
 $SERVER &
 PID0=$!
+
+sleep 2
+
+echo CLIENT1
 
 cd $TMP/client1
 
@@ -35,10 +41,14 @@ $CLIENT list 127.0.0.1
 $CLIENT run 127.0.0.1 &
 PID1=$!
 
+echo CLIENT2
+
 cd $TMP/client2
 $CLIENT get 127.0.0.1 0
 $CLIENT run 127.0.0.1 &
 PID2=$!
+
+echo CLIENT3
 
 cd $TMP/client3
 $CLIENT get 127.0.0.1 0
@@ -49,9 +59,13 @@ sleep 10
 
 FILE_SUFFIX=downloads/0/`basename $REFERENCE_FILE`
 
-
+echo CMP1
 cmp --silent $TMP/client2/$FILE_SUFFIX $REFERENCE_FILE || echo "Bad file content for client 2"
+
+echo CMP2
 cmp --silent $TMP/client3/$FILE_SUFFIX $REFERENCE_FILE || echo "Bad file content for client 3"
+
+echo KILL
 
 kill $PID0 $PID1 $PID2 $PID3
 kill -9 $PID0 $PID1 $PID2 $PID3
